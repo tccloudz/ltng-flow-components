91bd71c8dc9a561c31d801f5366ce673fab24f41
Terence Chiu <Terence Chiu>
2018-10-03 09:54:06 -0400

    Updated FlowRecEditCmp to include lightning:recordEditForm base component in order to support specifying specific edit fields to be loaded onto the flow screen by providing a comma-separated set of field API names of the sObject. Javascript controller updated with a handler method to process the list of fields provided via visual workflow. Component design file updated to include additional parameters needed to be accessible from flow.

--- aura/FlowRecordEditCmp/FlowRecordEditCmpController.js
+++ aura/FlowRecordEditCmp/FlowRecordEditCmpController.js
@@ -1,5 +1,24 @@
 ({
-	saveRecord : function(component, event, helper) {
+	
+    doInit: function(component, event, helper){
+        
+        let formType = component.get('v.editFormType');
+        let obj = component.get('v.objApiName');
+        
+        console.log(obj);
+        
+        if(formType === 'recordForm'){
+			let fields = component.get('v.fieldList');
+            console.log('field list: ' + fields);
+            if(fields){
+           		let fieldsArr = fields.split(',');
+                console.log(fieldsArr);
+            	component.set('v.fieldListArr', fieldsArr);
+            }
+        }
+    },
+    
+    saveRecord : function(component, event, helper) {
         
         console.log('saving record');
         component.find("recEdit").get("e.recordSave").fire();
@@ -7,7 +26,9 @@
     
     saveSuccessful : function(component, event, helper){
         console.log('handling successful save');
-    	component.set('v.showMsg', true);
+        let showMsg = component.get('v.showMsg');
+        component.set('v.showMsg', true);
+        
     },
     
 })
